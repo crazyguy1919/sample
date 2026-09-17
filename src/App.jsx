fdacc7a91b4e2505638ca20d79ce645e4f4ba311
saimedicover <142868103+crazyguy1919@users.noreply.github.com>
2026-09-17 14:39:45 +0530

    Update welcome message in App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Welcome</h1>
+          <h1>Welcome updated</h1>
          
         </div>
        
